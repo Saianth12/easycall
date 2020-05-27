4945678d0492fb1abfe87ee09d3138c5a6296032
Sainath12 <sainathkishorerajagopal@gmail.com>
2020-05-27 17:17:29 +0530

    withdb

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 	$servername = "localhost";
-	$username = "root";
-	$password = '';
-	$dbname = "easycalls";
+	$username = "sssdneer_dneers";
+	$password = 'DneersCom@123_$';
+	$dbname = "sssdneer_mobileapp";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
